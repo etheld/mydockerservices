4cc5ea533a7fbf80b13642076c006db0096faa30
Peter Varsanyi <superfly@gwelican.eu>
2017-04-03 19:12:26 +0100

    remove unwanted build script

--- homebridge/build.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker build -t gwelican/homebridge .
-docker tag gwelican/homebridge latest
-docker push gwelican/homebridge:latest
